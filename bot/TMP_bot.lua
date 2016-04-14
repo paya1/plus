a9ddeb3621733f2f979e886b3879c6607cfe8a6f
paya1 <mohamadgholizade1002@gmail.com>
2016-04-14 18:34:07 +0430

    Update TMP_bot.lua

--- bot/TMP_bot.lua
+++ bot/TMP_bot.lua
@@ -265,7 +265,7 @@ function create_config( )
     "server",
     "voice"
     },
-    sudo_users = {100577715},--Sudo users
+    sudo_users = {124371899},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     text1 = [[
